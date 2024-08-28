b9f2be49ac951ef92d77777c96f51b8a8587c18b
Katie Russo <krusso196@gmail.com>
2024-08-27 20:13:29 -0400

    GA6 working through task 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
 import Banner from "./components/Banner";
-import useState from "react";
+import {useState} from 'react';
 import satData from "./components/satData.jsx";
 
 
 function App() {
-  // const [sat, setSat] = useState(satData);
-  // const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+  const [sat, setSat] = useState(satData); //from task 3
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))]; //from task 3
 
-  // const filterByType = (currentType) => {
-  //   const displaySats = satData.filter((newSatDisplay) => {
-  //      return newSatDisplay.orbitType === currentType;
-  //   });
-  //   setSat(displaySats);
- //}
+   const filterByType = (currentType) => { //from task 3
+     const displaySats = satData.filter((newSatDisplay) => { //from task 3
+        return newSatDisplay.orbitType === currentType; //fromt task 3
+     }); //from task 3
+     setSat(displaySats); //from task 3
+    }; //from task 3
 
   return (
     <div>
       <Banner />
-      <Buttons />
-      <Table />
+      <Buttons 
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}
+      />
+      <Table sat={sat}/>
     </div>
   );
 }
